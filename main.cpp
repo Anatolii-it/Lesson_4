699b7dc12951b7f558d95e6dfcf04b8ec899deff
Anatolii-it <t0663261690@gmail.com>
2024-03-06 10:45:42 +0200

    task1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <Windows.h>
-#include <vector>
 #include <conio.h>
 
 using namespace std;
@@ -8,26 +7,27 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    
+
     int n;
     cout << "Введіть кількість елементів масиву: ";
     cin >> n;
 
-    vector<int> array(n);
+    int* array = new int[n];
 
-    cout << "Введіть перше число: ";
+    cout << "Введіть перший елемент: ";
     cin >> array[0];
 
     for (int i = 1; i < n; ++i) {
         array[i] = array[i - 1] * 2;
     }
 
-    cout << "Заданий масив: ";
+    cout << "Отриманий масив: ";
     for (int i = 0; i < n; ++i) {
         cout << array[i] << " ";
     }
     cout << endl;
-    
+
+    delete[] array;
 
     _getch();
     return 0;
